eda4539920681f5fe44ba5f58d0b2afed719718b
cndnhere <cndn.here@gmail.com>
2022-02-26 01:20:42 +0530

    done

--- javascript1.js
+++ javascript1.js
@@ -15,8 +15,11 @@ function changeMode(n)
 {
     if(n==1)
     {
-        document.getElementById("changemode").setAttribute("style","background:black; color:white");
+        document.getElementById("changemode").setAttribute("style","background:black;");
+        document.getElementsByClassName("mode")[0].setAttribute("style","color:white");
     }else{
-        document.getElementById("changemode").setAttribute("style","background:#F0FFF0; color: black")
+        document.getElementById("changemode").setAttribute("style","background:#F0FFF0;");
+        document.getElementsByClassName("mode")[0].setAttribute("style","color:black");
+
     }
 }
